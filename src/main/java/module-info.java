f99438993ea9cd39065072ca662d1922f7b6dc53
DamianLubkowski <multiclick@wp.pl>
2022-09-22 20:45:44 +0200

    Punkt 5

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,8 @@ module pl.lublin.wsei.java.cwiczenia.lab6 {
     requires javafx.fxml;
 
 
-    opens pl.lublin.wsei.java.cwiczenia.lab6 to javafx.fxml;
-    exports pl.lublin.wsei.java.cwiczenia.lab6;
+    opens pl.lublin.wsei.java.cwiczenia.console to javafx.fxml;
+    exports pl.lublin.wsei.java.cwiczenia.console;
     exports pl.lublin.wsei.java.cwiczenia;
     opens pl.lublin.wsei.java.cwiczenia to javafx.fxml;
 }
